voxpupuli/voxpupuli-release

commit 12be4ba3dfe236a69de873f8d8e5c424b7a140d1

Cleanup references to Travis CI in rake tasks
This makes the real task "release" and makes "travis_release" an alias
for it. It also sets the description of "travis_release" to a message
stating that there is a newer task named "release"

The readme is also updated to reflect the change in task used. Some
additional formatting and cleanup was also applied to the readme.

lib/voxpupuli/release/rake_tasks.rb
@@ -1,8 +1,7 @@
 require 'puppet_blacksmith/rake_tasks'
 
-desc 'release new version through Travis-ci'
-task "travis_release" do
-
+desc 'Release via GitHub Actions'
+task :release do
   Blacksmith::RakeTask.new do |t|
     t.build = false # do not build the module nor push it to the Forge
     t.tag_sign = true # sign release with gpg
@@ -28,14 +27,14 @@ task "travis_release" do
   ENV['BLACKSMITH_FULL_VERSION'] = v_new
   Rake::Task["module:bump:full"].invoke
 
-  # push it out, and let travis do the release:
+  # push it out, and let GitHub Actions do the release:
   g.commit_modulefile!(v_new)
   g.push!
 end
 
-desc 'Release via GitHub Actions (alias for travis_release)'
-task :release do
-  Rake::Task['travis_release'].invoke
+desc 'Depreciated: use the "release" task instead'
+task "travis_release" do
+  Rake::Task['release'].invoke
 end
 
 desc 'Check Changelog.'

commit cd0ac11bcaa59ed4992c2ce04a35c3f10815297b

Warn the user the travis_release should not be used
Fixes: 12be4ba3dfe236a69de873f8d8e5c424b7a140d1

lib/voxpupuli/release/rake_tasks.rb
@@ -34,6 +34,7 @@ end
 
 desc 'Depreciated: use the "release" task instead'
 task "travis_release" do
+  STDERR.puts "Depreciated: use the 'release' task instead"
   Rake::Task['release'].invoke
 end
 